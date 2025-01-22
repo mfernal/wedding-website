89ef0e77515730ef1ff7e074d0554a7c009d0e48
Manuel Fernandez <manfer.alonso@gmail.com>
2025-01-22 19:32:22 +0100

    feat: create about

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>Un poco sobre nosotros</h1>
+            <p>¡Ainara es una mimimi de manual!</p>
+        </main>
+    )
+}
+
+export default AboutPage
